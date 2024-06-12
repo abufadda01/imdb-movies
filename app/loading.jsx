54234390cee2624f49918421ae70bb02c6782ddc
laith abufadda <laith.abufadda@Eranovel.local>
2024-06-12 10:07:13 +0300

    loading effect

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex justify-center mt-10'>
+        <img className='h-60' src="spinner.svg" alt="loading..." />
+    </div>
+  )
+}
+
+export default loading
